2a885b45d082f1204e944ab8dde0d43e732d15b3
mbuzzas <mbuzzas3@gmail.com>
2017-03-03 14:51:16 -0800

    adds database.js

---
+++ app/models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+	local: {
+		email: String,
+		password: String,
+	}
+});
+
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://user:user123@ds161179.mlab.com:61179/blogging-app' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
