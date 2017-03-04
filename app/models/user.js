fb6d846b2172b9d45943d746edb487d5f07fec2c
mbuzzas <mbuzzas3@gmail.com>
2017-03-03 16:19:25 -0800

    adds passport.js

--- app/models/user.js
+++ app/models/user.js
@@ -8,3 +8,12 @@ var userSchema = mongoose.Schema({
 	}
 });
 
+userSchema.methods.generateHash = function(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+	return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
